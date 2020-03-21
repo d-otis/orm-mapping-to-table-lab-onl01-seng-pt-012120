f523d10eedfe261c30acd928d9075542c1de90c2
Dan Foley <dan@dan-foley.com>
2020-03-21 17:32:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,7 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
   end
   
   def self.create_table
